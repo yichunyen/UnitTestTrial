ac913045199828484852beeabdbba4e4f8119c23
Yi-Chun <yenyichun.developer@gmail.com>
2020-01-19 19:59:47 +0800

    UserTest: implement BaseTest to read JSON file.

--- app/src/test/java/yichunyen/unit/test/trial/UserTest.kt
+++ app/src/test/java/yichunyen/unit/test/trial/UserTest.kt
@@ -8,24 +8,11 @@ import java.io.FileInputStream
 import java.io.InputStreamReader
 
 
-class UserTest {
-    private val ASSET_BASE_PATH = "../app/src/main/assets/"
-    private fun readJsonFile(filename: String): String {
-        val br = BufferedReader(InputStreamReader(FileInputStream(ASSET_BASE_PATH + filename)))
-        val sb = StringBuilder()
-        var line = br.readLine()
-        while (line != null) {
-            sb.append(line)
-            line = br.readLine()
-        }
-
-        return sb.toString()
-    }
-
+class UserTest : BaseTest() {
     @Test
     fun userTest(){
         val gson = GsonBuilder().create()
-        val user = gson.fromJson(readJsonFile("user.json"), User::class.java)
+        val user = gson.fromJson(readJsonFile("user"), User::class.java)
         Assert.assertEquals("Tester", user.name)
         println("id = ${user.id}")
         println("name = ${user.name}")
